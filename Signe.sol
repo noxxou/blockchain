48ab571f93c628d4301d328b46b66744d7ad6ce5
Alexandre Lenfantin <alex@Noxsous-Mac.local>
2024-03-21 08:09:47 +0100

    modif

--- Signe.sol
+++ Signe.sol
@@ -2,27 +2,28 @@
 pragma solidity ^0.8.17;
 // We have to specify what version of compiler this code will compile with
 contract Signe {
-    mapping (bytes32 => uint256) public votesReceived;
-    bytes32[] public fileHash;
-    constructor(bytes32[] memory fileHashs) {
-    fileList = fileHashs;
-}
-// This function returns the total votes a candidate has received so far
-function totalVotesFor(bytes32 filehash) view public returns (uint256) {
-    require(validCandidate(candidate));
-    return votesReceived[candidate];
-}
-function voteForCandidate(bytes32 filehash) public {
-    require(validCandidate(candidate));
-    votesReceived[candidate] += 1;s
-}
 
-function validFile(bytes32 filehash) view public returns (bool) {
-    for(uint i = 0; i < candidateList.length; i++) {
-        if (candidateList[i] == candidate) {
-            return true;
+    struct FileStruct{
+        uint id;
+        string hash_string;
+        address[] signataires;
+    } 
+    uint public nbFile;
+    mapping (uint => FileStruct) public FileList;
+    uint[] public fileIdList;
+
+    function addFile(uint new_id, string memory new_hash) public {
+        fileIdList.push(new_id);
+        FileList[nbFile] = FileStruct(new_id, new_hash,new address[](0));
+    }
+
+    function addSign(uint new_id, address signataire) public {
+    for(uint i = 0; i < fileIdList.length; i++) {
+        if (FileList[i].id == new_id) {
+            FileList[i].signataires.push(signataire);
+            break;
         }
     }   
-    return false;
-    }
+}
+        
 }
