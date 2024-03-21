ca434a3f0bbbb79eb5e57ae9f3819c6a8211f970
Alexandre Lenfantin <alex@Noxsous-Mac.local>
2024-03-21 08:31:56 +0100

    fin smart contract

--- Signe.sol
+++ Signe.sol
@@ -12,18 +12,45 @@ contract Signe {
     mapping (uint => FileStruct) public FileList;
     uint[] public fileIdList;
 
-    function addFile(uint new_id, string memory new_hash) public {
+    function addFile(uint new_id, string memory new_hash)public {
         fileIdList.push(new_id);
         FileList[nbFile] = FileStruct(new_id, new_hash,new address[](0));
     }
 
-    function addSign(uint new_id, address signataire) public {
-    for(uint i = 0; i < fileIdList.length; i++) {
-        if (FileList[i].id == new_id) {
-            FileList[i].signataires.push(signataire);
-            break;
+    function addSign(uint new_id, address signataire)public {
+        for(uint i = 0; i < fileIdList.length; i++) {
+            if (FileList[i].id == new_id) {
+                FileList[i].signataires.push(signataire);
+                break;
+            }
         }
-    }   
-}
-        
+    }
+    
+    function verifyFile(string memory new_hash) view public returns(bool) {
+        for(uint i = 0; i < fileIdList.length; i++) {
+            if (compareStrings(FileList[fileIdList[i]].hash_string, new_hash)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function verifySign(string memory new_hash, address check_sign) view public returns(bool){
+        for(uint i = 0; i < fileIdList.length; i++) {
+            if (compareStrings(FileList[fileIdList[i]].hash_string, new_hash)) {
+                for (uint j = 0; j < FileList[fileIdList[i]].signataires.length; j++) {
+                    if (FileList[fileIdList[i]].signataires[j] == check_sign) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        return false;
+        }
+
+    function compareStrings(string memory a, string memory b) internal pure returns (bool) {
+        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
+    }
+
 }
